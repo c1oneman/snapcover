3695ee450aa6b220467b25f4abbb0c6cbd991461
Clayton Loneman <clayloneman@gmail.com>
2019-06-07 08:40:32 -0700

    Initial Commit

---
+++ SnapNP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapNP
+//
+//  Created by Clay Loneman on 6/7/19.
+//  Copyright © 2019 Clay Loneman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
